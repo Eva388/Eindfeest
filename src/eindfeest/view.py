cc8e481d7f1911451f10d114fa7a6009d798304e
M-spree <135598804+M-spree@users.noreply.github.com>
2023-12-07 10:13:50 +0100

    Better commit file

---
+++ src/eindfeest/view.py
@@ -0,0 +1,28 @@
+#Made by Melle Sprey 14026376
+from matplotlib import pyplot as plt
+import csv  
+from eindfeest.diode_experiment import DiodeExperiment
+
+
+
+
+def do_your_thing_python():
+    """The View part of the arduino code. Takes from the model DiodeExperiment, and performs the measurements (with low = 0, high =1023 , n = 10),
+     plots the measurements and saves them to a csv file called 'metingen'
+    """
+    data = DiodeExperiment()                #Does the thing
+    Vav,Verr,Iav,Ierr = data.scan(0,1023,10)    
+    plt.errorbar(Vav,Iav,Ierr,Verr,fmt='none')      #Plotting with our errorbars
+    plt.title('UI curve of LED')
+    plt.ylabel('I (Ampere)')
+    plt.xlabel('U (Volt)')
+    plt.show()
+
+    zippert = zip(Vav,Verr,Iav,Ierr)        #zips arrays
+    with open('metingen.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['V','Verr','I','Ierr'])  #First line, and the rest of the data trough for loop
+        for i in zippert:                   
+            writer.writerow(i)
+
+
